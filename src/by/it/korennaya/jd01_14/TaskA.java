b8c87479f4247c474b17edd69610d5d3a51ec046
Anna Sergienko <anna.sergienko@devim.team>
2022-07-01 00:32:01 +0300

    01_14_TaskAcorrected

--- src/by/it/korennaya/jd01_14/TaskA.java
+++ src/by/it/korennaya/jd01_14/TaskA.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class TaskA {
+
+    public static final String USER_DIR = "user.dir";
     public static final String DATA_TASK_A_BIN = "dataTaskA.bin";
     public static final String RESULT_TASK_A_TXT = "resultTaskA.txt";
 
